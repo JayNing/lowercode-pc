5c689e60a65056713b378f3b4a4315e904b05d1e
JianjianNing <472420531@qq.com>
2023-04-12 09:59:10 +0800

    gateway集成

---
+++ lowercode-gateway/src/main/java/com/jayning/lowercode/lowercodegateway/LowercodeGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.jayning.lowercode.lowercodegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LowercodeGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LowercodeGatewayApplication.class, args);
+    }
+
+}
---
+++ lowercode-gateway/src/test/java/com/jayning/lowercode/lowercodegateway/LowercodeGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayning.lowercode.lowercodegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LowercodeGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
